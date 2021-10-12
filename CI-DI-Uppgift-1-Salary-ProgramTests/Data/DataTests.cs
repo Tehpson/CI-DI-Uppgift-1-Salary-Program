84d9932fbe533f37fc0f068725d81f353fcb8ef3
AlbinJGitHub <72981304+AlbinJGitHub@users.noreply.github.com>
2021-10-12 11:12:50 +0200

    mer test fixes

---
+++ CI-DI-Uppgift-1-Salary-ProgramTests/Data/DataTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using CI_DI_Uppgift_1_Salary_Program.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CI_DI_Uppgift_1_Salary_Program.Data.Tests
+{
+    [TestFixture()]
+    public class DataTests
+    {
+        [Test()]
+        public void FillDataTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
